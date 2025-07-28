8dedfb0f29f3fbde73aded046e50b0248d46ffe8
Shravan0001jha <shravan0001jha@gmail.com>
2025-07-28 12:29:58 +0530

    Redirection

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -124,8 +124,8 @@
                                 <div class="fs-3 fw-light text-muted">Empowering learners to achieve more with </div>
                                 <h1 class="display-3 fw-bolder mb-5"><span class="text-gradient d-inline">Expert Tutors and Quality contents</span></h1>
                                 <div class="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xxl-start mb-3">
-                                    <a class="btn btn-primary btn-lg px-5 py-3 me-sm-3 fs-6 fw-bolder" href="#">Explore Courses</a>
-                                    <a class="btn btn-outline-dark btn-lg px-5 py-3 fs-6 fw-bolder" href="#">Book a free demo</a>
+                                    <a class="btn btn-primary btn-lg px-5 py-3 me-sm-3 fs-6 fw-bolder" href="https://educationalavenue.in/">Explore Courses</a>
+                                    <a class="btn btn-outline-dark btn-lg px-5 py-3 fs-6 fw-bolder" href="https://educationalavenue.in/">Book a free demo</a>
                                 </div>
                             </div>
                         </div>
